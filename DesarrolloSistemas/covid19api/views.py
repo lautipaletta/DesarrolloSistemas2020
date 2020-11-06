36e82ba908da5b08e505dd7e4a023c54cb4ecffd
Fabrizio <fabrizio.carlassara@gmail.com>
2020-11-06 17:17:24 -0300

    Sistema de logueo

--- DesarrolloSistemas/covid19api/views.py
+++ DesarrolloSistemas/covid19api/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+
+from . models import User
 
 # Create your views here.
-def index (request):
-    pass
+def index(request):
+    
+    if request.method == "POST":
+
+        username = User.objects.get(email=request.POST["email"]).username
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            return render(request, "covid19api/index.html")
+
+        else:
+            return render(request, "covid19api/login.html")
+
+    else:
+
+        user = User.objects.get(username=request.user)
+
+        if user.is_authenticated:
+            return render(request, "covid19api/index.html")
+        else:
+            return render(request, "covid19api/login.hmtl")
